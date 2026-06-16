4a49367b5efcf8767efc6c73c372af31c3e7d056
Vinicius Alves <viniciusalvestech@gmail.com>
2026-06-16 11:39:40 -0300

    fix: usar unicode escape na regex de diacríticos em utils/slug

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -4,7 +4,7 @@ export function slugify(texto: string): string {
   return texto
     .toLowerCase()
     .normalize('NFD')
-    .replace(/[̀-ͯ]/g, '')
+    .replace(/[\u0300-\u036f]/g, '')
     .replace(/[^a-z0-9]+/g, '-')
     .replace(/^-+|-+$/g, '');
 }
